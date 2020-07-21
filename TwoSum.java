d876d761ea1340ec92c7c6e77b49c43172594a78
Akhil Sudhakaran <akhil.sudh@gmail.com>
2020-07-21 23:15:05 +0530

    Two sum solution
    https://leetcode.com/problems/two-sum/

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                int j = map.get(target - nums[i]);
+                if(i != j) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {-1};
+    }
+}
