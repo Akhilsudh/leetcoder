ca70a3347660cdb8f2999dfbd9b9bee5cafda6be
Akhil Sudhakaran <akhil.sudh@gmail.com>
2020-07-22 00:25:14 +0530

    Longest substring without repeating characters
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,20 @@
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> substr = new HashSet<Character>();
+        int max = 0;
+        int i = 0, j = 0, size= s.length();
+        
+        while(i < size && j < size){
+            if(!substr.contains(s.charAt(j))) {
+                substr.add(s.charAt(j));
+                j++;
+                max = (max > j - i) ? max : j - i;
+            }
+            else {
+                substr.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return max;
+    }
+}
