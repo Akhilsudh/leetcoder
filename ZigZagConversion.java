5b3857cc5bba1e933bcb85a5c0b2732333e87c88
Akhil Sudhakaran <Akhil.sudh@gmail.com>
2020-07-25 23:45:54 +0530

    Update ZigZagConversion.java

--- ZigZagConversion.java
+++ ZigZagConversion.java
@@ -1,59 +1,36 @@
 public class ZigZagConversion {
     public String convert(String s, int numRows) {
-        if(numRows == 0 || numRows == 1 || s.length() <= numRows) {
+        if(numRows < 2){
             return s;
         }
-        boolean flag = true;
-        int row = 0, a = 2 * (numRows - 1) - 1, b = -1, i = 0;
-        String res = "";
-        while(row < numRows) {
-            if(b == -1){
-                if(i >= s.length()){
-                    b += 2;
-                    a -= 2;
-                    row++;
-                    i  = row;
+        String[] rows = new String[numRows];
+        Arrays.fill(rows, "");
+        int rowNum = 0;
+        boolean directionDown = true;
+        int i = 0;
+        int n = s.length();
+        while(i < n) {
+            if(directionDown) {
+                rows[rowNum] = rows[rowNum] + s.charAt(i);
+                i++;
+                if(rowNum == numRows - 1) {
+                    rowNum--;
+                    directionDown = false;
                     continue;
                 }
-                res = res + s.charAt(i);
-                i += a + 1;
+                rowNum++;
             }
-            else if(a == -1){
-                if(i >= s.length()){
-                    row++;
-                    i  = row;
+            else {
+                rows[rowNum] = rows[rowNum] + s.charAt(i);
+                i++;
+                if(rowNum == 0){
+                    rowNum++;    
+                    directionDown = true;
                     continue;
                 }
-                res = res + s.charAt(i);
-                i += b + 1;
-            }
-            
-            else if(flag) {
-                if(i >= s.length()){
-                    a -= 2;
-                    b += 2;
-                    row++;
-                    i  = row;
-                    continue;
-                }
-                flag = false;
-                res = res + s.charAt(i);
-                i += a + 1;
-            }
-            else if(!flag) {
-                if(i >= s.length()){
-                    a -= 2;
-                    b += 2;
-                    row++;
-                    i  = row;
-                    flag = true;
-                    continue;
-                }
-                flag = true;
-                res = res + s.charAt(i);
-                i += b + 1;
+                rowNum--;
             }
         }
-        return res;
+        return String.join("", rows);
     }
 }
