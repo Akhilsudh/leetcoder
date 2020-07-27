9fc3664c1e3e1a3b81d39eddd9d174420d350bbb
Akhil <akhil.sudh@gmail.com>
2020-07-27 22:50:39 +0530

    Better solution added
    https://leetcode.com/problems/palindrome-number/

--- PalindromNumber.java
+++ PalindromNumber.java
@@ -1,14 +1,13 @@
 public class PalindromNumber {
     public boolean isPalindrome(int x) {
-        if(x < 0) {
+        if((x < 0) || ((x % 10 == 0) && (x != 0))) {
             return false;
         }
-        int xCopy = x;
         int rev = 0;
-        while(xCopy != 0) {
-            rev = rev * 10 + xCopy % 10;
-            xCopy /= 10;
+        while(x > rev) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
-        return (rev == x)?true:false;
+        return ((rev == x) || (rev / 10 == x)) ? true : false;
     }
 }
