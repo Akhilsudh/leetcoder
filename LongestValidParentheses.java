e9eb89609d60c97411fb9ceb8304f90646f3c79d
Akhil <akhil.sudh@gmail.com>
2020-08-21 01:41:17 +0530

    Optimised solution for Longest Valid Parentheses

--- LongestValidParentheses.java
+++ LongestValidParentheses.java
@@ -1,19 +1,20 @@
 public class LongestValidParentheses {
     public static int longestValidParentheses(String s) {
-        int length = s.length();
+        char[] string = s.toCharArray();
+        int length = string.length;
         int max = 0;
         int[] lookupTable = new int[length];
-        for(int i = 0; i < length; i++) {
-            if(s.charAt(i) == ')' && i > 0) {
-                if(s.charAt(i - 1) == '(') {
+        for(int i = 1; i < length; i++) {
+            if(string[i] == ')') {
+                if(string[i - 1] == '(') {
                     lookupTable[i] += 2;
-                    if(i >= 2) {
+                    if(i > 1) {
                         lookupTable[i] += lookupTable[i - 2];
                     }
                 }
-                else if(i - lookupTable[i - 1] >= 1 && s.charAt(i - lookupTable[i - 1] - 1) == '(') {
+                else if(i - lookupTable[i - 1] > 0 && string[i - lookupTable[i - 1] - 1] == '(') {
                     lookupTable[i] = lookupTable[i - 1] + 2;
-                    if(i - lookupTable[i - 1] >= 2) {
+                    if(i - lookupTable[i - 1] > 1) {
                        lookupTable[i] += lookupTable[i - lookupTable[i - 1] - 2];
                     }
                 }
@@ -25,5 +26,7 @@ public class LongestValidParentheses {
 
     public static void main(String args[]) {
         System.out.println(longestValidParentheses("()"));
+        System.out.println(longestValidParentheses(")()(()))"));
+        System.out.println(longestValidParentheses("))(("));
     }
 }
