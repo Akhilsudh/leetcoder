ac4078f0e173b689ff47c25d297f75ba86fa5dd6
Akhil <akhil.sudh@gmail.com>
2020-08-21 21:24:37 +0530

    Added recursive solution to reverse k group list

---
+++ ReverseNodesInkGroup.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseNodesInkGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        return head;
+    }
+    public ListNode recursiveFunction(ListNode node, int k) {
+        if(node == null) {
+            return node;
+        }
+        int count = 0;
+        ListNode temp = node;
+        for(int i = 0; i < k; i++) {
+            if(temp == null) {
+                break;
+            }
+            temp = temp.next;
+            count++;
+        }
+        if(count != k) {
+            return node;
+        }
+        ListNode destination = recursiveFunction(temp, k);
+        ListNode current = node;
+        while(current != temp) {
+            ListNode ahead = current.next;
+            current.next = destination;
+            destination = current;
+            current = ahead;
+        }
+        return destination;
+    }
+}
