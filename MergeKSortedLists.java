297d67b3f84f139370002a78c6c5d68cff59fe2e
Akhil <akhil.sudh@gmail.com>
2020-08-23 23:28:35 +0530

    Update

--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,5 +1,35 @@
 import java.util.Comparator;
 import java.util.PriorityQueue;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class MergeKSortedLists {
-    PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(Comparator.<ListNode>comparingInt(o -> o.val));
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(Comparator.<ListNode>comparingInt(obj -> obj.val));
+        for(ListNode item: lists) {
+            if(item != null) {
+                pq.add(item);
+            }
+        }
+        ListNode head = new ListNode();
+        ListNode temp = head;
+        ListNode poll = pq.poll();
+        while(poll != null) {
+            temp.next = new ListNode();
+            temp = temp.next;
+            temp.val = poll.val;
+            if(poll.next != null) {
+                pq.add(poll.next);
+            }
+            poll = pq.poll();
+        }
+        return head.next;
+    }
 }
