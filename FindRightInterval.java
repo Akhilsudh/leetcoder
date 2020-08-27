caa16280931878ab9ca3f17f98316f9ddd4e1a2d
Akhil Sudhakaran <akhil.sudh@gmail.com>
2020-08-28 01:52:03 +0530

    TreeMap solution top Find the right interval

--- FindRightInterval.java
+++ FindRightInterval.java
@@ -1,22 +1,20 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class FindRightInterval {
     public int[] findRightInterval(int[][] intervals) {
         int[] result = new int[intervals.length];
-        int right, left, min, minj;
-        for(int i = 0; i < intervals.length; i++){
-            right = intervals[i][1];
-            min = Integer.MAX_VALUE; 
-            minj = -1;
-            for(int j = 0; j < intervals.length; j++) {
-                if(j == i) {
-                    continue;
-                }
-                left = intervals[j][0];
-                if((left - right > -1) && ((left - right) < min)) {
-                    min = left - right;
-                    minj = j;
-                }
+        TreeMap<Integer, Integer> rightIntervalStartValueIndex = new TreeMap<Integer, Integer>();
+        for(int i = 0; i < intervals.length; i++) {
+            rightIntervalStartValueIndex.put(intervals[i][0], i);
+            result[i] = -1;
+        }
+        Integer key;
+        for(int i = 0; i < intervals.length; i++) {
+            key = rightIntervalStartValueIndex.ceilingKey(intervals[i][1]);
+            if(key != null) {
+                result[i] = rightIntervalStartValueIndex.get(key);
             }
-            result[i] = minj;
         }
         return result;
     }
