bbaf44974e9b3f7ecda392153d2485384a1f0d6c
Akhil Sudhakaran <akhil.sudh@gmail.com>
2020-09-01 23:57:15 +0530

    Added more optimized solution for permutation with xor element swapping

--- Permutation.java
+++ Permutation.java
@@ -3,29 +3,37 @@ import java.util.*;
 public class Permutation {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Set<Integer> numbers = new HashSet<Integer>();
-        for(int num: nums) {
-            numbers.add(num);
-        }
-        backtrack(result, new ArrayList<Integer>(), numbers);
+        backtrack(result, nums, 0);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> list, Set<Integer> numbers) {
-        if(numbers.isEmpty()) {
-            result.add(new ArrayList<>(list));
+    private void backtrack(List<List<Integer>> result, int[] nums, int index) {
+        if(index == nums.length - 1 || nums.length == 0) {
+            List<Integer> res = new ArrayList<Integer>();
+            for(int i = 0; i < nums.length; i++) {
+                res.add(nums[i]);
+            }
+            result.add(res);
             return;
         }
         else {
-            for(int number: numbers) {
-                Set<Integer> newNumbers = new HashSet<Integer>(numbers);
-                newNumbers.remove(number);
-                list.add(number);
-                int index = list.size() - 1;
-                backtrack(result, list, newNumbers);
-                list.remove(index);
+            for(int i = index; i < nums.length; i++) {
+                swap(i, index, nums);
+                backtrack(result, nums, index + 1);
+                swap(i, index, nums);
             }
             return;
         }
     }
+
+    private void swap(int index1, int index2, int[] numbers) {
+        if(index1 == index2) {
+            return;
+        }
+        numbers[index1] = numbers[index1] ^ numbers[index2];
+        numbers[index2] = numbers[index1] ^ numbers[index2];
+        numbers[index1] = numbers[index1] ^ numbers[index2];
+        return;
+    }
+
 }
