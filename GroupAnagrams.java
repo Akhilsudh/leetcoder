3e1ae220075b2d56cd072cad997c628b689f589c
Akhil Sudhakaran <akhil.sudh@gmail.com>
2020-09-17 17:57:24 +0530

    Best solution I could come with for GroupAnagrams

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -26,7 +26,9 @@ public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> results= new HashMap<String, List<String>>();
         for(String word: strs) {
-            String key = generateKey(word);
+            char[] keyArr = word.toCharArray();
+            Arrays.sort(keyArr);
+            String key = keyArr.toString();
             if(!results.containsKey(key)) {
                 List<String> list = new ArrayList<String>();
                 list.add(word);
@@ -37,12 +39,43 @@ public class GroupAnagrams {
         }
         return new ArrayList<List<String>>(results.values());
     }
-    private String generateKey(String word) {
-        int[] charSet = new int[26];
-        char[] wordArr = word.toCharArray();
-        for(char letter: wordArr) {
-            charSet[letter - 'a']++;
-        }
-        return Arrays.toString(charSet);
-    }
 }
+
+/*  An alternate solution that I found online (Using prime numbers as factors for one of each character) further reducing
+    time for converting back to string.
+     __________________________________________________________________________________________________________________
+    |   import java.util.*;                                                                                            |
+    |   public class GroupAnagrams {                                                                                   |
+    |       static long [] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101};      |
+    |                                                                                                                  |
+    |       long getEncoding(String s) {                                                                               |
+    |           char[] stringChars = s.toCharArray();                                                                  |
+    |           long hashCode = 1;                                                                                     |
+    |           for (char c : stringChars) {                                                                           |
+    |               hashCode *= primes[c - 'a'];                                                                       |
+    |           }                                                                                                      |
+    |           return hashCode;                                                                                       |
+    |       }                                                                                                          |
+    |                                                                                                                  |
+    |       public List<List<String>> groupAnagrams(String[] strs) {                                                   |
+    |           Map<Long, List<String>> anagrams = new HashMap<>();                                                    |
+    |           List<List<String>> result = new ArrayList<>();                                                         |
+    |                                                                                                                  |
+    |           for (String str: strs) {                                                                               |
+    |               long encoding = getEncoding(str);                                                                  |
+    |               if(anagrams.containsKey(encoding)) {                                                               |
+    |                   anagrams.get(encoding).add(str);                                                               |
+    |               }                                                                                                  |
+    |               else {                                                                                             |
+    |                   List<String> l = new ArrayList<>();                                                            |
+    |                   l.add(str);                                                                                    |
+    |                   result.add(l);                                                                                 |
+    |                   anagrams.put(encoding, l);                                                                     |
+    |               }                                                                                                  |
+    |           }                                                                                                      |
+    |                                                                                                                  |
+    |           return result;                                                                                         |
+    |       }                                                                                                          |
+    |   }                                                                                                              |
+    |__________________________________________________________________________________________________________________|
+*/
