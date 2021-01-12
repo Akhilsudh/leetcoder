0f4066929665f198dd42e34a7d0afe0b8bbb5076
Akhil Sudhakaran <akhil.sudh@gmail.com>
2021-01-12 17:58:52 +0530

    Updated Add twoo numbers solution with the daily challenge code

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,20 @@
+/*  https://leetcode.com/problems/add-two-numbers/
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, 
+    and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    Example 1:
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+    Example 2:
+        Input: l1 = [0], l2 = [0]
+        Output: [0]
+    Example 3:
+        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        Output: [8,9,9,9,0,0,0,1]
+*/
 
- // Definition for singly-linked list.
+// Definition for singly-linked list.
 class ListNode {
     int val;
     ListNode next;
@@ -9,29 +24,57 @@ class ListNode {
 }
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int digit1 = 0, digit2 = 0, sum = 0, carry = 0;
-        ListNode sumList = new ListNode(), lastRef = null, ref = sumList;
-        while((l1 != null) || (l2 != null) || (carry != 0)) {
-            digit1 = 0; digit2 = 0;
-            if(l1 != null) {
-                digit1 = l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null) {
-                digit2 = l2.val;
-                l2 = l2.next;
-            }
-            sum = digit1 + digit2 + carry;
-            carry = sum / 10;
-            sum = sum % 10;
-            ref.val = sum;
-            lastRef = ref;
-            ref.next = new ListNode();
-            ref = ref.next;
-        }
-        if(ref.val == 0) {
-            lastRef.next = null;
-        }
-        return sumList;
+        ListNode result = new ListNode();
+        result.val = 0;
+        addListNodes(l1, l2, result, 0);
+        return (result.next == null) ? result : result.next;
     }
+
+    void addListNodes(ListNode l1, ListNode l2, ListNode result, int carry) {
+        int a = (l1 != null) ? l1.val : 0;
+        int b = (l2 != null) ? l2.val : 0;
+        if(l1 == null && l2 == null && carry == 0) {
+            result.next = null;
+        }
+        else {
+            result.next = new ListNode();
+            result = result.next;
+            result.val = (a + b + carry) % 10;
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+            addListNodes(l1, l2, result, (a + b + carry) / 10);
+        }
+        return;
+    }
+
+    ListNode createListNodes(String number) {
+        ListNode list = new ListNode();
+        ListNode temp = list;
+        for(int i = number.length() - 1; i >= 0; i--) {
+            temp.next = new ListNode(Integer.parseInt(number.charAt(i) + ""));
+            temp = temp.next;
+        }
+        return list.next;
+    }
+
+    void printNumber(ListNode list) {
+        String result = "";
+        while(list != null) {
+            result = list.val + result;
+            list = list.next;
+        }
+        System.out.println(result);
+        return;
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers obj = new AddTwoNumbers();
+        ListNode l1 = obj.createListNodes("342");
+        ListNode l2 = obj.createListNodes("465");
+        obj.printNumber(obj.addTwoNumbers(l1, l2));
+        l1 = obj.createListNodes("9999999");
+        l2 = obj.createListNodes("9999");
+        obj.printNumber(obj.addTwoNumbers(l1, l2));d
+    }
+
 }
