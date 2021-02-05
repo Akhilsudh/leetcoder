00a6752ff5429a9ce15a47c97502ae1aad19b43f
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2021-02-05 23:53:46 +0530

    Added base code for simplify path

---
+++ SimplifyPath.java
@@ -0,0 +1,12 @@
+/*
+*/
+import java.util.*;
+
+public class SimplifyPath {
+  public String simplifyPath(String path) {
+    String[] pathElements = path.split("/");
+    List<String> canonicalPath = new ArrayList<String>();
+    for(String element: pathElements) {
+        
+    }
+}
